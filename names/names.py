bc9327eb55fdb9dc52a4579d37833ded9ac637e5
Joseph <j.wagner1024@gmail.com>
2019-10-18 13:33:45 -0400

    Complete Sprint Challenge tasks, and pass all tests.

--- names/names.py
+++ names/names.py
@@ -1,5 +1,6 @@
 import time
 
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -11,10 +12,11 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# Design based on LRU Cache
+dict_comp = {name:1 for name in names_1}
+for name in names_2:
+    if name in dict_comp.keys():
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
