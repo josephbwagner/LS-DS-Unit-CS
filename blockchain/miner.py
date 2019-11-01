d66a67a2a4052f57a547388007783d138eea879c
Joseph Wagner <j.wagner1024@gmail.com>
2019-11-01 13:53:29 -0400

    Complete blockchain task. Still mining due to Heroku instability.

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -24,8 +24,9 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
-    #  TODO: Your code here
+    proof = 1_000_000
+    while valid_proof(last_proof, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -39,8 +40,13 @@ def valid_proof(last_hash, proof):
     IE:  last_hash: ...AE9123456, new hash 123456888...
     """
 
-    # TODO: Your code here!
-    pass
+    last_hash = str(last_hash)
+    guess = f'{last_hash}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    # print(f'last_hash {last_hash}')
+    # print(f'guess {guess}')
+    # print(f'guess_hash {guess_hash}')
+    return guess_hash[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
